0871b3cce41013e1f1f0d4304ccce48aef6892b4
Siddhesh Desai <siddhesh1500@rediffmail.com>
2024-06-01 06:23:53 +0000

    All work done again

---
+++ src/Components/Education.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2>Education</h2>
+      <h4>Advance Diploma in Software Engineering</h4>
+      <h5>E-Connect Computer Education</h5>
+      <h6>Duration 2023 - 2024</h6>
+    
+      <h4>Senior Secondary</h4>
+      <h5>National Institute of Open Schooling (NIOS)</h5>
+      <h6>Duration 2015 - 2020</h6>
+
+      <h4>Secondary</h4>
+      <h5>National Institute of Open Schooling (NIOS)</h5>
+      <h6>Duration 2013 - 2015</h6>
+    </section>
+  );
+};
+
+export default Education;
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,10 +1,10 @@
 /**
  * Portfolio component
  *
- * Highlights some of  your creations. These can be designs, websites,
+ * Highlights some of your creations. These can be designs, websites,
  * open source contributions, articles you've written and more.
  *
- * This is a great area for you to to continually add to and refine
+ * This is a great area for you to continually add to and refine
  * as you continue to learn and create.
  */
 
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images//laptop-notepad-wooden-desk.jpg";
+import image from "../images/laptop-notepad-wooden-desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -33,7 +33,7 @@ const projectList = [
   {
     title: "Calculator",
     description:
-      "Designed and implemented a caalculator application.",
+      "Designed and implemented a calculator application.",
     url: "https://mycalculator-101.netlify.app/",
   },
   {
@@ -64,7 +64,12 @@ const Portfolio = () => {
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{
+              height: "90%",
+              width: "100%",
+              objectFit: "cover",
+              animation: "1s ease-out 0s 1 slideInLeft",
+            }}
             alt={imageAltText}
           />
         </div>
